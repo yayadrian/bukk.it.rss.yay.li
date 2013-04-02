3e195a5846c8e470303f68ff26cbeb081976d88e
Adrian Lansdown <ael@thejaronline.com>
2013-04-02 21:31:58 +0100

    Changed from using JSDom to Cheerio
    Cheerio is faster and works on Windows

--- make.js
+++ make.js
@@ -3,8 +3,9 @@ console.log("running");
 (function RSSfile() {
 
 // make the rss feed
-	var jsdom = require('jsdom')
-	, request = require('request');
+	var cheerio = require('cheerio'),
+		request = require('request'),
+		RSS = require('rss');
 
 	var numberOfItems = 50;
 
@@ -18,85 +19,69 @@ console.log("running");
 			console.log('Request error.');
 			return false;
 		}
-								//Send the body param as the HTML code we will parse in jsdom
-			//also tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
-			jsdom.env({
-												html: body,
-												scripts: ['http://code.jquery.com/jquery-1.6.min.js']
-								}, function(err, window){
-				 //Use jQuery just as in a regular HTML page
-											
-												var $ = window.jQuery;
-												
 
-												var RSS = require('rss');
+		$ = cheerio.load(body);
+		
+		/* lets create an rss feed */
+		var feed = new RSS({
+						title: 'bukk.it RSS',
+						description: 'An RSS Feed for Bukk.it',
+						feed_url: 'http://bukk.it.rss.yay.li/rss.xml',
+						site_url: 'http://bukk.it',
+						image_url: 'http://bukk.it/favicon.ico',
+						author: 'Bukk.it'
+				});
 
-												/* lets create an rss feed */
-												var feed = new RSS({
-																title: 'bukk.it RSS',
-																description: 'An RSS Feed for Bukk.it',
-																feed_url: 'http://example.com/rss.xml',
-																site_url: 'http://bukk.it',
-																image_url: 'http://bukk.it/favicon.ico',
-																author: 'Bukk.it'
-														});
+		$links = $('tr');
+		var num = 0;
 
-												$links = $('tr');
-												var num = 0;
+		$links.each(function (i, item) {
+			// limit to 20 items for now
+			if(i >= numberOfItems) { return false; }
 
-												$links.each(function (i, item) {
-													// limit to 20 items for now
-													if(i >= numberOfItems) { return false; }
+			console.log($(item).find('td a').text());
 
-													console.log($(item).find('td a').text());
+			var fileName = $(item).find('td a').text();
+			var fileURL  = "http://bukk.it/" + fileName;
+			var fileDate = $(item).find('td:nth-child(3)').text();
+			var fileSize = $(item).find('td:nth-child(4)').text();
 
-													var fileName = $(item).find('td a').text();
-													var fileURL  = "http://bukk.it/" + fileName;
-													var fileDate = $(item).find('td:nth-child(3)').text();
-													var fileSize = $(item).find('td:nth-child(4)').text();
+			if(fileName && fileURL && fileDate && fileSize ) {
+				// 
+				
+				var badHTML = '<h1>' + fileName + '</h1><img src="' + fileURL +'" /> <p>Size:' + fileSize + '</p>';
+				badHTML += '<a href="' + fileURL +'">' + fileURL + '</a>';
+				
+			/* loop over data and add to feed */
 
-													if(fileName && fileURL && fileDate && fileSize ) {
-														// 
-														
-														var badHTML = '<h1>' + fileName + '</h1><img src="' + fileURL +'" /> <p>Size:' + fileSize + '</p>';
-														badHTML += '<a href="' + fileURL +'">' + fileURL + '</a>';
-														
-													/* loop over data and add to feed */
+				feed.item({
+						title:  fileName,
+						description: badHTML,
+						url: fileURL, // link to the item
+						guid: fileURL, // optional - defaults to url
+						author: 'bukk.it', // optional - defaults to feed author property
+						date: fileDate // any format that js Date can parse.
+				});
+				
+				num++;
+			}
+		});
+		
 
-														feed.item({
-																title:  fileName,
-																description: badHTML,
-																url: fileURL, // link to the item
-																guid: fileURL, // optional - defaults to url
-																author: 'bukk.it', // optional - defaults to feed author property
-																date: fileDate // any format that js Date can parse.
-														});
-														
-														num++;
-													}
-												});
-												
+		var fs = require('fs');
+		var fileName = "savedRSS.xml";
 
-												var fs = require('fs');
-												// var DateFormat = require('dateformatjs').DateFormat;
-												// var df = new DateFormat("\yyyyMMMdd_HHmmss");
-												// var fileName = 'savedRSS\\' + df.format(new Date()) + '.xml'; 
-												var fileName = "/home/yayadrian/webapps/bukkitrss/savedRSS.xml";
+		fs.writeFile(fileName, feed.xml(), function(err) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log("The file was saved!");
+				var date = new Date();
+				console.log("File updated: " + date);
+			}
+		});
 
-												fs.writeFile(fileName, feed.xml(), function(err) {
-												    if(err) {
-												        console.log(err);
-												    } else {
-												        console.log("The file was saved!");
-														var date = new Date();
-														console.log("File updated: " + date);
-												    }
-												}); 
-
-
-
-								});
-	};
-})()
+	}
+})();
 
 //exports.RSSfile = RSSfile;
